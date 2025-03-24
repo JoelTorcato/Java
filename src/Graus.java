548dc9200ee616492702fa9456c1fa802202d8d1
JoelTorcato <a12022@ae4outubro.pt>
2025-03-24 19:13:06 +0000

    New commit.

---
+++ src/Graus.java
@@ -0,0 +1,12 @@
+public class Graus {
+    public static void main (String[] args) {
+
+        final int a = 22;
+        final int b = 25;
+        final int c = 27;
+
+        System.out.println("Porto "+a+" graus \n ");
+        System.out.println("Lisboa "+b+" graus \n");
+        System.out.println("Faro "+c+" graus \n");
+    }
+}
