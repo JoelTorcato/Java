993a610770c3453907ea5160f00e011af7bd5387
JoelTorcato <a12022@ae4outubro.pt>
2025-04-01 12:25:09 +0100

    New commit.

---
+++ src/Leitura_escrita.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Leitura_escrita {
+    public static void main(String[] args) {
+        int idade;
+        String nome;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Nome: ");
+        nome = input.next();
+        System.out.print("Idade: ");
+        idade = input.nextInt();
+        System.out.print("O " + nome + " tem " + idade + " anos");
+
+        input.close();
+    }
+}
